86f2e6a978e25ac24544f67304307d03c68951dd
Koen Basten <kbasten@gmail.com>
2013-06-10 00:58:27 +0200

    Documentation! :D

--- RoomFull/RoomFull/RoomFull.cs
+++ RoomFull/RoomFull/RoomFull.cs
@@ -26,6 +26,8 @@ namespace RoomFull
 
         public override MethodDefinition[] GetHooks(TypeDefinitionCollection scrollsTypes, int version)
         {
+            // hook to Communicator.sendRequest(Message msg);
+            // only hook to the method with the Message parameter, don't care about the String version
             return new MethodDefinition[] { scrollsTypes["Communicator"].Methods.GetMethod("sendRequest", new Type[]{typeof(Message)}) };
         }
 
@@ -38,13 +40,22 @@ namespace RoomFull
             returnValue = null;
             Message m = (Message)info.Arguments()[0];
 
+            // check whether it's a RoomEnterMessage, leave the RoomEnterFreeMessage because
+            // we still want to join the first free room upon starting the game
             if (m is RoomEnterMessage && !(m is RoomEnterFreeMessage))
             {
                 RoomEnterMessage rem = (RoomEnterMessage)m;
 
+                // construct new RoomEnterMultiMessage with desired room
                 RoomEnterMultiMessage remm = new RoomEnterMultiMessage(new String[]{ rem.roomName });
+                // and send the request to the server.
                 App.Communicator.sendRequest((Message)remm);
+
+                // add the room to the UI, this isn't done automatically after RoomEnterMulti, 
+                // only after RoomEnter which this isn't.
                 App.ArenaChat.ChatRooms.SetCurrentRoom(rem.roomName);
+
+                // ... and stop the method from executing :)
                 return true;
             }
 
